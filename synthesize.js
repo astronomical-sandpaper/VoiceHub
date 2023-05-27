0244336422ec70cdfbba0a2c7861cebb7b2bd21e
Junior-Is-Great67 <65474160+Junior-Is-Great67@users.noreply.github.com>
2023-05-27 17:18:26 -0400

    TTS maker because im so tired of putting it in HTML

---
+++ synthesize.js
@@ -0,0 +1,26 @@
+function generateSpeech(message, voice)
+{
+  					let fakeEmail = [];
+						for (let c = 0; c < 15; c++) fakeEmail.push(~~(65 + Math.random() * 26));
+						const email = `${String.fromCharCode.apply(null, fakeEmail)}@gmail.com`;
+	
+						const q = new URLSearchParams({						
+							msg: text,
+							voice: voice.arg,
+							email
+						}).toString();
+						
+						https.get({
+							hostname: "api.voiceforge.com",
+							path: `/swift_engine?${q}`,
+							headers: { 
+								'User-Agent': 'just_audio/2.7.0 (Linux;Android 11) ExoPlayerLib/2.15.0',
+								HTTP_X_API_KEY: '8b3f76a8539',
+								'Accept-Encoding': 'identity',
+								'Icy-Metadata': '1',
+							 }
+						}, (r) => {
+							return r;
+						});
+						break;
+}
